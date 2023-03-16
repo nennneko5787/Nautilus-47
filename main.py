f08f0f85269a3f7ac78c26cca866499ca3708057
Nennneko5787 <nennneko@tensi.org>
2023-03-16 17:29:50 +0900

    /salmon_run_indeed3コマンドのブキの画像が邪魔なので削除

--- main.py
+++ main.py
@@ -110,19 +110,25 @@ async def salmon_run_indeed3(ctx: discord.Interaction):
 	embed2=discord.Embed(title="ステージ", color=0x9B95C9) #埋め込みの説明に、メッセージを挿入し、埋め込みのカラーを紫`#9B95C9`に設定
 	embed2.add_field(name=stage['name'], value="Stage id:{}".format(stage['id']))
 	embed2.set_image(url=stage['image'])
-	embed3=discord.Embed(title="ブキ①", color=0x9B95C9) #埋め込みの説明に、メッセージを挿入し、埋め込みのカラーを紫`#9B95C9`に設定
-	embed3.add_field(name=weapons[0]['name'], value="")
-	embed3.set_image(url=weapons[0]['image'])
-	embed4=discord.Embed(title="ブキ②", color=0x9B95C9) #埋め込みの説明に、メッセージを挿入し、埋め込みのカラーを紫`#9B95C9`に設定
-	embed4.add_field(name=weapons[1]['name'], value="")
-	embed4.set_image(url=weapons[1]['image'])
-	embed5=discord.Embed(title="ブキ③", color=0x9B95C9) #埋め込みの説明に、メッセージを挿入し、埋め込みのカラーを紫`#9B95C9`に設定
-	embed5.add_field(name=weapons[2]['name'], value="")
-	embed5.set_image(url=weapons[2]['image'])
-	embed6=discord.Embed(title="ブキ④", color=0x9B95C9) #埋め込みの説明に、メッセージを挿入し、埋め込みのカラーを紫`#9B95C9`に設定
-	embed6.add_field(name=weapons[3]['name'], value="")
-	embed6.set_image(url=weapons[3]['image'])
-	await ctx.response.send_message(embeds=[embed, embed2, embed3, embed4, embed5, embed6]) #メッセージを送信
+	embed3=discord.Embed(title="ブキ", color=0x9B95C9)
+	embed3.add_field(name="ブキ①", value=weapons[0]['name'])
+	embed3.add_field(name="ブキ②", value=weapons[1]['name'])
+	embed3.add_field(name="ブキ③", value=weapons[2]['name'])
+	embed3.add_field(name="ブキ④", value=weapons[3]['name'])
+	#embed3=discord.Embed(title="ブキ①", color=0x9B95C9) #埋め込みの説明に、メッセージを挿入し、埋め込みのカラーを紫`#9B95C9`に設定
+	#embed3.add_field(name=weapons[0]['name'], value="")
+	#embed3.set_image(url=weapons[0]['image'])
+	#embed4=discord.Embed(title="ブキ②", color=0x9B95C9) #埋め込みの説明に、メッセージを挿入し、埋め込みのカラーを紫`#9B95C9`に設定
+	#embed4.add_field(name=weapons[1]['name'], value="")
+	#embed4.set_image(url=weapons[1]['image'])
+	#embed5=discord.Embed(title="ブキ③", color=0x9B95C9) #埋め込みの説明に、メッセージを挿入し、埋め込みのカラーを紫`#9B95C9`に設定
+	#embed5.add_field(name=weapons[2]['name'], value="")
+	#embed5.set_image(url=weapons[2]['image'])
+	#embed6=discord.Embed(title="ブキ④", color=0x9B95C9) #埋め込みの説明に、メッセージを挿入し、埋め込みのカラーを紫`#9B95C9`に設定
+	#embed6.add_field(name=weapons[3]['name'], value="")
+	#embed6.set_image(url=weapons[3]['image'])
+	#await ctx.response.send_message(embeds=[embed, embed2, embed3, embed4, embed5, embed6]) #メッセージを送信
+	await ctx.response.send_message(embeds=[embed, embed2, embed3]) #メッセージを送信
 
 @client.event
 async def on_ready():
